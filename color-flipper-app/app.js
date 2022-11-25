ae813ace67c58dff3a7fff81ad18e43d8d4fafd6
diken.dev <diken.dev@gmail.com>
2022-11-25 15:19:13 -0300

    randon function generator thougt hex array

--- color-flipper-app/app.js
+++ color-flipper-app/app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "yellow", "blue", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "yellow", "blue", "gray", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
@@ -8,8 +8,11 @@ btn.addEventListener('click', function() {
    console.log(randomNumber);
    document.body.style.backgroundColor = colors[randomNumber];
    color.textContent = colors[randomNumber];
+   btn.style.backgroundColor = colors[randomNumber];
 });
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);    
 }
+
+color.style.color = "rgb(162, 169, 177)";
